a49f76a38d5a31c1fd40a7344eb406a11c8c1a88
Uday Mehtani <udaymehtani@gmail.com>
2022-12-20 18:52:06 +0530

    Initial Commit

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,4 +1,61 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract ETHDaddy {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721 {
+    // string public name;
+    // string public symbol;
+    // here we will use "name" and "symbol" variables from our ERC721.sol contract
+
+    address public owner;
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol) {
+        // passing same arguements in constructor of ERC721 contract 
+        owner = msg.sender;
+    }
+    uint256 public maxSupply  = 0;
+    uint256 public totalSupply = 0;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You don't have access to this function!");
+        _;
+    }
+    struct Domain {
+        string name;
+        uint256 cost;
+        bool isOwned;
+    }
+
+    mapping (uint256 => Domain) public domains;
+    function listDomain(string memory _name, uint256 _cost) public onlyOwner{
+        domains[0]   =  Domain(_name, _cost, false);
+        maxSupply++;
+    }
+
+    function getDomain(uint256 _id) public view returns(Domain memory){
+        return domains[_id];
+    }
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function mint(uint256 _id) public payable {
+        require(_id <= maxSupply, "Invalid Request");
+        require(domains[_id].isOwned == false, "This is already owned by someone else");
+        require(msg.value > domains[_id].cost, "You do not have sufficient funds!");
+        _safeMint(msg.sender, _id);
+        domains[_id].isOwned = true;
+        totalSupply++;
+        // getDomain(_id).isOwned = true; // for some weried reason, this does not work 
+    }
+
+    function withdraw() public onlyOwner {
+        // payable(owner).transfer(address(this).balance);
+        // this also works
+        (bool success, ) = owner.call{value: address(this).balance}("we can write anything here while sending funds to owner");
+        // calls CALL method in global variable OWNER and if it returns true in success, transaction gets completed
+        require(success);
+}
+}
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,115 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
+// a function to convert ETH to WEI
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
+  let eTHDaddy // cannot name it to const as we need to declare the value while declaring const variables
+let NAME = "ETHDaddy"
+let SYMBOL = "ETHD"
 
+  beforeEach(async function(){
+    // let deployer, owner1;
+    [deployer, NFTowner1] = await ethers.getSigners() // contains an array of 20 objects for 20 different accounts
+    
+    const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
+    eTHDaddy = await ETHDaddy.deploy(NAME, SYMBOL);
+    await eTHDaddy.deployed(); 
+    const transaction = await eTHDaddy.connect(deployer).listDomain("uday.eth", tokens(10))
+    // here deployer will be the owner of the smart contract
+    await transaction.wait()
+  })
+  describe("Deployment", async function(){
+    it('it has a name', async function(){
+      expect(await eTHDaddy.name()).to.equal(NAME)
+    })
+  
+    it("it has a symbol", async function(){
+      expect(await eTHDaddy.symbol()).to.equal(SYMBOL)
+  
+    })
+
+    it("checks the owner", async function(){
+      expect(await eTHDaddy.owner()).to.equal(deployer.address)
+    })
+
+    it("checking max supply after deploying the contract", async function(){
+      expect(await eTHDaddy.maxSupply()).to.equal(1)
+      // 1 because we have made a transaction to this function in beforeEach hook
+    })
+
+    it("checking total supply after deploying the contract", async function(){
+      expect(await eTHDaddy.totalSupply()).to.equal(0)
+      // 0 because we have not minted any NFT yet
+    })
+
+  })
+
+  describe("Domain", async function(){
+    it("returns domains attributes", async function(){
+      let domain = await eTHDaddy.getDomain(0) // this case does not work without using "domain" variable
+      expect(await domain.name).to.equal("uday.eth") // this works with "to.be" also
+      expect(await domain.cost).to.equal(tokens(10))
+      // actually this is passed in as "10000000000000000000" WEI and not 10000000000000000000 WEI
+      expect(await domain.isOwned).to.equal(false)
+    })
+  })
+
+  describe("Minting", async function(){
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits('10', 'ether')
+    beforeEach(async function(){
+      const txn = await eTHDaddy.connect(NFTowner1).mint(ID, {value: AMOUNT})
+      await txn.wait()
+    })
+    it("updates the owner", async function(){
+      // ID = 1, because we are buying the first domain name we listed using "await eTHDaddy.connect(deployer).listDomain("uday.eth", tokens(10))"
+      expect(NFTowner1.address).to.equal(await eTHDaddy.ownerOf(ID))
+      // ownerOf(ID) is a function of ERC721 contract which returns the address of account who called that safeMint function for that ID.
+    })
+
+    it("updates domain status", async function() {
+      const domain = await eTHDaddy.getDomain(ID) 
+      expect(domain.isOwned).to.equal(true)
+    })
+
+    it("updates contract balance", async function() {
+        expect(await eTHDaddy.getBalance()).to.equal(AMOUNT)
+    })
+
+    it("updating total supply after minting the NFT", async function(){
+      expect(await eTHDaddy.totalSupply()).to.equal(1)
+      // 1 because we have minted 1 NFT now
+    })
+  })
+
+  describe("Withdrawing", () => {
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("10", 'ether')
+    let balanceBefore
+
+    beforeEach(async () => {
+    balanceBefore = await ethers.provider.getBalance(deployer.address) // balance of smart contract's owner before withdrawal
+
+    // minting token ID 1 by NFTowner1
+    let transaction = await eTHDaddy.connect(NFTowner1).mint(ID, { value: AMOUNT })
+    await transaction.wait()
+
+    transaction = await eTHDaddy.connect(deployer).withdraw()
+    await transaction.wait()
+    })
+
+    it('updates the owner balance', async () => {
+    const balanceAfter = await ethers.provider.getBalance(deployer.address)
+    expect(balanceAfter).to.greaterThan(balanceBefore)
+    })
+
+    it('updates the contract balance', async () => {
+    const result = await eTHDaddy.getBalance()
+    expect(result).to.equal(0)
+    })
+    })
 })
